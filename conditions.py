d4dab42381045c415104ca4e0e720501b639c2c4
rayyan22207 <rayyan22207@gmail.com>
2024-08-26 15:38:38 +0500

    simplest version

---
+++ conditions.py
@@ -0,0 +1,8 @@
+def condtion(value):
+    if value == "Hello":
+        return "Hello"
+    elif (value == "Weather" or value == "what is the weather"):
+        return "weather_api"
+    else:
+        return "Sorry, I don't understand"
+    
---
+++ main.py
@@ -0,0 +1,12 @@
+import conditions
+
+def main():
+    input_value = input("Enter a value: ")
+    result = conditions.condtion(input_value)
+    print(result)
+
+
+
+
+if __name__ == "__main__":
+    main()

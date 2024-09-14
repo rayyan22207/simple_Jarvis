3a57729ced0e5057c17177ad09ac0e98439f656e
rayyan22207 <rayyan22207@gmail.com>
2024-09-15 01:25:26 +0500

    working on

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def main():
     while True:
         user_input = input("Enter a value (or 'exit' to quit): ")
         result = conditions.condition(user_input)
-        if result.startswith("Sorry"):
-            user_input = input("Please enter something else (or 'exit' to quit): ")
-            print(result)
-        else:
-            print(result)
+        # if result.startswith("Sorry"):
+        #     user_input = input("Please enter something else (or 'exit' to quit): ")
+        #     print(result)
+        # else:
+        print(result)
         if user_input.lower() == "exit":
             break
 
